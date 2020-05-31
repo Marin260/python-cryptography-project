3b1c8842863381e979520942e8082d617ef86538
Marin260 <marin.martuslovic@gmail.com>
2020-05-31 23:15:09 +0200

    cd prvi dio zavrsen

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ trenutno_vrijeme = strftime("%d.%m.%Y. %H:%M:%S", localtime()) #Vrijeme
 print('Pozdrav, dobro dosao...')
 print('Datum i vrijeme vaseg pristupa: {}'.format(trenutno_vrijeme))
 
-korisnik, host, adresa, kucni_dir = os.getlogin(), socket.gethostname(), os.path.abspath(os.getcwd()), os.getenv("HOME")
+kucni_dir = os.getenv("HOME")
 
 povijest = kucni_dir + '/.povijest'
-print(povijest)
 while True: #Petlja koja vrti prompt
+    korisnik, host, adresa = os.getlogin(), socket.gethostname(), os.path.abspath(os.getcwd())
     naredba = input('[{0}@{1}:{2}]$ '.format(korisnik, host, adresa)) #Prompt/odzivni znak
     if naredba == 'exit' or naredba == 'logout':
         break;
@@ -62,17 +62,37 @@ while True: #Petlja koja vrti prompt
         
     elif naredba == "kill":
         print(naredba)
-    elif naredba == "cd":
-        print(naredba)
-    elif re.match(r"(date\s+.*)|(date$)", naredba): #date naredba
+    elif re.match(r"(cd\s+.*)|(cd$)", naredba): #cd naredba
+        def korak_nazad(adresa):
+            trenutna = adresa.split('/')
+            nova_adresa = ""
+            for ele in trenutna[1:-1]:
+                nova_adresa += "/"
+                nova_adresa += ele
+            return nova_adresa
+            
+        if re.match(r"^cd\s*$", naredba):
+            os.chdir(kucni_dir)
+        elif re.match(r"cd\s\.{1}\s*$", naredba):
+            continue
+        elif re.match(r"cd\s\.{2}\s*", naredba):
+            os.environ['prethodna'] = korak_nazad(adresa)
+            os.chdir(os.environ['prethodna'])
+
+            
+        
+    elif re.match(r"(date\s.*)|(date\s*$)", naredba): #date naredba
         if re.match(r"date\s*$", naredba):
-                print (time.strftime("%H::%M::%S  %A  %d/%m/%Y"))
-                upis_u_dat(naredba, povijest)
+            print (strftime("%H::%M::%S  %A  %d/%m/%Y"))
+            upis_u_dat(naredba, povijest)
         elif re.match(r"date -r\s*$", naredba):
-                print (time.strftime("%d/%m/%Y  %A  %H::%M::%S"))
-                upis_u_dat(naredba, povijest)
+            print (strftime("%d/%m/%Y  %A  %H::%M::%S"))
+            upis_u_dat(naredba, povijest)
+        elif re.match(r"date -[^r]\s*$", naredba):
+            print('Nepostojeci parametar')
         else:
-                print('Nepostojeci parametar ili argument')
+            print('Nepostojeci argument')
+    
     elif naredba == "ls":
         print(naredba)
     elif naredba == "mkdir":
