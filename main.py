e2bb424ec2a2ec5679bb846a84df01db8f0d63ed
Marin260 <60893098+Marin260@users.noreply.github.com>
2020-05-31 15:10:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import socket
 import os
 import re
 
+def upis_u_dat(var, ime_dat): #upis naredbi u dat
+    dat = open(ime_dat, 'a')
+    dat.write(var)
+    dat.write('\n')
+    dat.close()
+
 trenutno_vrijeme = strftime("%d.%m.%Y. %H:%M:%S", localtime()) #Vrijeme
 
 print('Pozdrav, dobro dosao...')
@@ -25,34 +31,34 @@ while True: #Petlja koja vrti prompt
     if re.match(r"(pwd\s+.*)|(pwd$)", naredba): #pwd naredba
         if re.match(r"pwd\s*$", naredba):
             print(os.path.abspath(os.getcwd()))
-            dat = open(povijest, 'a')
-            dat.write(naredba)
-            dat.write('\n')
-            dat.close()
+            upis_u_dat(naredba, povijest)
         else:
             print('Naredba ne prima parametre ni argumente')
             
     elif re.match(r"(ps\s+.*)|(ps$)", naredba): #ps naredba
         if re.match(r"ps\s*$", naredba):
             print(os.getpid())
-            dat = open(povijest, 'a')
-            dat.write(naredba)
-            dat.write('\n')
-            dat.close()
-            
+            upis_u_dat(naredba, povijest)
         else:
             print('Nepostojeci parametar ili argument')
             
-    elif re.match(r"(echo\s+.*)|(echo$)", naredba):
+    elif re.match(r"(echo\s+.*)|(echo$)", naredba): #echo naredba
         if re.match(r"echo\s*$", naredba):
             print('Naredba prima barem jedan argument')
         else:
+            upis_u_dat(naredba, povijest)
             for word in lista_sa_naredbom[1:]:
-                if re.match(r'"^[^\s\"]+\"[^\s\"]+$"', word):
-                    print(word)
-                else:
+                if re.match(r'^\"(.*\".*)*\"$', word):
+                    word = word[1:-1]
+                    print(word, end=" ")
+                elif re.match(r'(^\"[^"]+)|([^"]+\"$)|(^\"[^"]+\"$)|(^\"$)', word):
                     izmjena = word.replace('"', '')
-                    print(izmjena)
+                    print(izmjena, end=" ")
+                else:
+                    print(word, end=" ")
+            print()
+            
+        
         
     elif naredba == "kill":
         print(naredba)
